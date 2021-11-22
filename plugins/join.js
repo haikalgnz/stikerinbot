f7a3f98675acc93c4fbd847488d233e21ec2bbcb
haikalgnz <haikalofc8@gmail.com>
2021-11-23 00:18:28 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,10 +1,11 @@
-let linkRegex = /([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com/\([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link Salah'
+    If (isMods || isOwner || m.frome) {
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => 
         var jumlahHari = 86400000 * 0.5
         var now = new Date() * 1
         if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
