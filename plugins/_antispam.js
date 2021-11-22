862e6f4b8f7d5b904a00817eedd83a8de837c2de
haikalgnz <haikalofc8@gmail.com>
2021-11-23 00:08:54 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,19 +1,14 @@
 let handler = m => m
 
 handler.all = async function (m) {
-    if (!db.data.settings[this.user.jid].antispam) return // antispam aktif?
-    if (m.isBaileys && m.fromMe) return
     if (!m.message) return
-    if (!m.isCommand) return
-    if (db.data.users[m.sender].banned) return
-    if (db.data.chats[m.chat].isBanned) return
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
-                db.data.users[m.sender].banned = true
-                await this.sendButton(m.chat, 'kamu dibanned karena spam!', '© stikerin', 'Pemilik Bot', ',owner', m)
+                //global.db.data.users[m.sender].banned = true
+                m.reply('*Jangan Spam!!*')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
