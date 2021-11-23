10e1083becca8e5c5580fbacc5b59c2c13390298
haikalgnz <haikalofc8@gmail.com>
2021-11-23 21:06:49 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,23 +1,33 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
-let handler = async (m, { conn, text, usedPrefix }) => {
+let handler = async (m, { conn, text, usedPrefix, participants, groupMetadata }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link Salah'
-    let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
-        var jumlahHari = 86400000 * 0.5
-        var now = new Date() * 1
-        if (now < global.db.data.chats[res.gid].expired) global.db.data.chats[res.gid].expired += jumlahHari
-        else global.db.data.chats[res.gid].expired = now + jumlahHari
+    if (!code) throw 'Link tidak valid atau tidak ada'
+    await m.reply('Bot akan masuk!')
+    let joiner = await conn.getName(m.sender)
+    let res = await conn.query({
+        json: ["action", "invite", code]
     })
-    await conn.sendButton(res.gid, `
-*${conn.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, *${conn.user.name}* diundang oleh @${m.sender.split`@`[0]}
-    
-ketik *${usedPrefix}menu* untuk melihat daftar perintah`.trim(), '© stikerin', 'Menu', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
+    if (res.status !== 200) throw `Tidak Dapat Masuk,, mungkin link telah direset ulang atau Bot telah dikeluarkan oleh seseorang.`
+    if (global.DATABASE.data.chats[m.chat].welcome) throw false
+    let grup = await conn.getName(res.gid)
+    let time = async (ms) => { return new Promise(resolve => setTimeout(resolve, ms)); }
+    let data = (await conn.groupMetadata(res.gid)).participants.map(u => u.jid)
+    let limit = 50
+    let member = data.length
+    if (member <= limit) {
+      conn.reply(res.gid, `Maaf ${joiner} Bot tidak dapat masuk,, Minimal Member harus ${limit} orang.`)
+      await time(3000)
+      conn.reply(res.gid, `Maaf Semua,, Bot akan keluar..`)
+      await time(2000)
+ await conn.groupLeave(res.gid)
+  } else if (member => limit) {
+     conn.reply(res.gid, `Hallo Member Grup\n*${grup}*\nAku telah ditambahkan oleh *${joiner}*\n\nKetik ${usedPrefix}menu untuk memulai Bot yaa`)
+ }
 }
-handler.help = ['join <chat.whatsapp.com>']
+handler.help = ['join <link gc>']
 handler.tags = ['info']
-handler.command = /^join$/i
+handler.command = /^(join)$/i
 
 handler.premium = false
 handler.owner = true
